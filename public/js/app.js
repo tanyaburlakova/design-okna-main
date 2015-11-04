227e86f8da0bc6cd8ee4f68326c0b6bec8f0126d
fatuk <fatukk@gmail.com>
2015-11-04 11:07:55 +0300

    Add compiling files

---
+++ public/js/app.js
@@ -0,0 +1,79 @@
+var app = angular.module('myApp', [
+		'ngRoute',
+		'mainCtrl'
+	])
+	.config([
+		'$routeProvider',
+
+		function ($routeProvider) {
+			'use strict';
+
+			/*$routeProvider
+				.when('/', {
+					redirectTo: '/main'
+				})
+				.when('/main', {
+					controller: 'MainCtrl',
+					templateUrl: 'views/main.html'
+				})
+				.otherwise({
+					redirectTo: '/main'
+				});*/
+		}
+	]);
+
+angular.module('mainCtrl', [])
+	.controller('MainCtrl', [
+		'$scope',
+		function ($scope) {
+			'use strict';
+
+		}
+	]);
+
+angular.module('someDirective', [])
+	.controller('SomeCtrl', [
+		'$scope',
+		function ($scope) {
+			'use strict';
+
+
+		}
+	])
+	.directive('some', function () {
+		'use strict';
+		return {
+			restrict: 'E',
+			controller: 'MainCtrl',
+			scope: {}
+		};
+	});
+
+app.service('ItemService', ['$http', '$q',
+	function ($http, $q) {
+		'use strict';
+		var item = this;
+		item.items = {};
+
+		filter.getItems = function () {
+			var url = 'api/url/',
+				defer = $q.defer();
+			$http.get(url)
+				.success(function (data) {
+					filter.types = data;
+					defer.resolve(data);
+				})
+				.error(function (data) {
+					defer.reject({
+						error: 'api access [%s] error!'.replace('%s', url)
+					});
+				});
+
+			return defer.promise;
+		};
+
+		return item;
+	}
+]);
+
+//# sourceMappingURL=../js/app.js.map
