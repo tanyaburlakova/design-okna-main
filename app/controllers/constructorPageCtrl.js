885e2ab77ad30619dc4034524e108b41aaf6bfa8
fatuk <fatukk@gmail.com>
2015-12-16 17:16:55 +0300

    Add tabs for contructor

--- app/controllers/constructorPageCtrl.js
+++ app/controllers/constructorPageCtrl.js
@@ -4,11 +4,39 @@
 		.controller('ConstructorPageCtrl', [
 			'$scope',
 			'$log',
+			'TexturesService',
 			constructorPageCtrl
 		]);
 
-	function constructorPageCtrl($scope, $log) {
+	function constructorPageCtrl($scope, $log, TexturesService) {
 		$log.log('Constructor page ctrl');
+
+		$scope.textureModel = null;
+
+		$scope.priceSlider = {
+			min: 100,
+			max: 180,
+			options: {
+				floor: 0,
+				ceil: 450
+			}
+		};
+
+		$scope.init = function () {
+			$scope.getTextures();
+		};
+
+		$scope.getTextures = function () {
+			TexturesService.getTextures()
+				.then(function (data) {
+					$scope.textures = data;
+				}, function (err) {
+					// Error
+					console.log(err);
+				});
+		};
+
+		$scope.init();
 
 	}
 
