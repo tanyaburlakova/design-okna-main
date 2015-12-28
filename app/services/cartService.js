6f34368d2211218d13e9fb41e6294331f0a02973
Victor Marchenko <viktor.marchenko@gmail.com>
2015-12-28 22:05:46 +0300

    Cart saves in localStorage

--- app/services/cartService.js
+++ app/services/cartService.js
@@ -8,7 +8,7 @@
 	]);
 
 	function cartService($rootScope, localStorageService) {
-		var products = [];
+		var products = JSON.parse(localStorageService.get('cart_products')) || [];
 		var service = {
 			addProduct: addProduct,
 			removeProduct: removeProduct,
@@ -17,11 +17,13 @@
 		return service;
 
 		function addProduct(product) {
-			products.push(prod);
+			products.push(product);
+			localStorageService.set('cart_products', JSON.stringify(products));
 		}
 
 		function removeProduct(idx) {
 			products.splice(idx, 1);
+			localStorageService.set('cart_products', JSON.stringify(products));
 		}
 
 		function getProducts(){
