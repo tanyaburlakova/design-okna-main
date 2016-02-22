b8ca4a90cec61484ca736a821df6d3cdff13af89
stanislavche <stress_tn@yahoo.com>
2016-02-22 13:33:55 +0300

    scroll fix

--- app/directives/cartDirective.js
+++ app/directives/cartDirective.js
@@ -20,14 +20,10 @@
 	function cartLink(scope, el, attr) {
 		var $dropdown = el.find('.cart__dropdown');
 		$dropdown.appendTo(angular.element('header.site-header'));
-		baron({
-			root: '.baron',
-			scroller: '.baron__scroller',
-			bar: '.baron__bar',
-			scrollingCls: '_scrolling',
-			draggingCls: '_dragging'
-		}).controls({
-			track: '.baron__track'
+		$('.js-customScroll').mCustomScrollbar({
+			axis:"y",
+			scrollbarPosition: "inside",
+			theme: 'dark-3'
 		});
 	}
 })();
