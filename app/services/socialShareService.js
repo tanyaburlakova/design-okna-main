1ac2c8dc1e83f3f0677209aedcda7b3561b9e696
Stanislav Alekseevich Chernyavskij <Stanislav Alekseevich Chernyavskij>
2016-04-20 20:13:27 +0300

    ios 8 bug

--- app/services/socialShareService.js
+++ app/services/socialShareService.js
@@ -16,7 +16,7 @@
 			switch(network){
 				case 'vk':
 					url = 'https://vk.com/share.php' + 
-						'?url=' + window.location.href +
+						'?url=' + encodeURIComponent(window.location.href) +
 						'&title=' + encodeURIComponent(params.title) +
 						'&description=' + encodeURIComponent(params.description) +
 						'&image=' + encodeURIComponent(params.image) +
@@ -25,13 +25,13 @@
 				case 'ok':
 					url = 'http://www.ok.ru/dk?st.cmd=addShare&st.s=1' +
 						'&st.comments=' + encodeURIComponent(params.description) +
-						'&st._surl=' + window.location.href
+						'&st._surl=' + encodeURIComponent(window.location.href)
 				break;
 				case 'fb':
 					url = 'http://www.facebook.com/sharer.php?s=100' +
 						'&p[title]=' + encodeURIComponent(params.title) +
 						'&p[summary]=' + encodeURIComponent(params.description) +
-						'&p[url]=' + window.location.href +
+						'&p[url]=' + encodeURIComponent(window.location.href) +
 						'&p[images][0]=' + encodeURIComponent(params.image)
 				break;
 				default:
